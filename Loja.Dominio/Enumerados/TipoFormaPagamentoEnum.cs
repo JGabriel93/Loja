c327f6052417cf7f0e68819c3892251695c4dfef
João Gabriel Andrade da Costa <joao.gabriel.andrade.costa@gmail.com>
2020-04-10 01:10:54 -0300

    feat(Enum): Inserir tipos de formas de pagamentos

---
+++ Loja.Dominio/Enumerados/TipoFormaPagamentoEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace Loja.Dominio.Enumerados
+{
+    public enum TipoFormaPagamentoEnum
+    {
+        Indefinido = 0,
+        Boleto = 1,
+        CartaoCredito = 2,
+        Deposito = 3
+    }
+}
